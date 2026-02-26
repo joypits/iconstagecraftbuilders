e4b9c339466737e4cb514339dea398dc6f6cff33
Joy Faith Prieto <joypitprieto@gmail.com>
2026-02-26 20:24:36 +0800

    first

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/iconstagecraftbuilders/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

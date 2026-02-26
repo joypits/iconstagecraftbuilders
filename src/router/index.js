e1ddb55c4ae51b166b8447304614f93533afb75a
Joy Faith Prieto <joypitprieto@gmail.com>
2026-02-26 20:38:02 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,16 @@ const routes = [
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+  scrollBehavior(to) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth'
+      }
+    }
+    return { top: 0 }
+  }
 })
 
 export default router

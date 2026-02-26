4094c52024a617078e2e10def66680fad8ab4f70
Joy Faith Prieto <joypitprieto@gmail.com>
2026-02-26 20:32:58 +0800

    index router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomePage from '@/views/HomePage.vue'
 import EquipmentDetails from '@/views/EquipmentDetails.vue'
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
